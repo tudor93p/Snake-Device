05d824526b2b293bcb2f4481ae92cbfc03b863c2
tudor <tudor.pahomi@gmail.com>
2021-08-22 00:00:06 +0200

    + local obs cut

--- src/TasksPlots.jl
+++ src/TasksPlots.jl
@@ -3,12 +3,15 @@ module TasksPlots
 
 import myPlots
 
+import myLibs: Utils 
+
 using myLibs.ComputeTasks: CompTask  
 using Helpers.Calculations: Calculation  
 using myPlots: PlotTask 
 
 using Constants: ENERGIES
 
+import ..Hamiltonian
 
 import ..LayeredLattice, ..GreensFcts, ..Hamilt_Diagonaliz
 
@@ -49,9 +52,73 @@ function LocalObservables(;observables::AbstractVector{<:AbstractString},
 	pt0 = Observables(;observables=observables, kwargs...)
 
 	return PlotTask(pt0,
-									myPlots.Sliders.init_localobs(observables),
+									(:localobs, observables),
 									myPlots.TypicalPlots.localobs(pt0, LayeredLattice)...)
 
+
+end
+
+#===========================================================================#
+#
+function LocalObservablesCut(;observables::AbstractVector{<:AbstractString},
+													kwargs...)#::PlotTask
+#
+#---------------------------------------------------------------------------#
+
+	pt0 = Observables(;observables=observables, kwargs...)
+
+	default_obs = myPlots.Sliders.pick_local(observables)[1] 
+
+	md,sd = myPlots.main_secondary_dimensions()
+
+	function plot(P)
+
+		@assert haskey(P, "Energy")
+
+		obs = get(P, "localobs", default_obs)
+
+		Data, good_P = pt0.get_data(P; mute=false, fromPlot=true,
+															    target=obs, get_good_P=true)
+
+		LDOS, labels = myPlots.Transforms.convol_energy(P, Data[obs], obs;
+													 Data=Data)  
+
+		Rs_MD, xs, inds = Hamiltonian.closest_to_dw(good_P...;
+															M=LayeredLattice, nr_curves=get(P,"region",1))
+
+
+		xs,ys,ylabels = Utils.zipmap(zip(Rs_MD,xs,inds)) do (R,x,i)
+
+			ldos, lab = myPlots.Transforms.closest_to_dw(P, LDOS[:]; R=R, inds=i)
+
+			(x, ldos), lab = myPlots.Transforms.transform(P, (x, ldos), lab)
+
+			return x, ldos, lab
+
+		end 
+
+
+		return Dict(
+
+			"xlabel" => "Coordinate \$$sd\$",
+
+			"ylabel" => myPlots.join_label(labels),
+
+			"labels" => myPlots.join_label.(ylabels),
+
+			"xs" => xs,
+
+			"ys" => ys,
+			
+			"xlim" => [minimum(minimum, xs), maximum(maximum, xs)],
+
+			)
+
+	end
+
+	return PlotTask(pt0, 
+									((:localobs, observables), (:regions, 8), (:transforms,)),
+									"LocalObservables_Cut", plot)
 
 end
 
--- test/plot.jl
+++ test/plot.jl
@@ -3,7 +3,7 @@ import Helpers, myPlots
 
 import Helpers.ObservableNames
 #using Helpers.Calculations: Calculation
-import myLibs: ComputeTasks, Parameters
+import myLibs: ComputeTasks, Parameters, Utils
 
 
 
@@ -16,11 +16,12 @@ PF = Helpers.hParameters.ParamFlow(Device.GreensFcts, get_input_dict(Device, tru
 
 
 tasks = D.([
-						:HParam,
+#						:HParam,
 #						:Latt,
-#						:LocalObservables,
-						:Observables,
-						:Spectrum,
+						:LocalObservables,
+						:LocalObservablesCut,
+#						:Observables,
+#						:Spectrum,
 						])
 
 #task = tasks[3]
@@ -50,9 +51,36 @@ println()
 
 #ComputeTasks.get_data_one(task, mute=false)
 
+P = (length = 75, Barrier_height = 2.0, Barrier_width = 0.03, SCDW_p = 2, SCDW_width = 0.005, SCDW_position = 0, SCpx_magnitude = 0.4, delta = 0.002, width = 37, SCpy_magnitude = 0.4)
+
+plot_P = OrderedDict{String, Any}("length" => 75, "Barrier_height" => 2.0, "Barrier_width" => 0.03, "SCDW_p" => 2, "SCDW_width" => 0.005, "SCDW_position" => 0, "SCpx_magnitude" => 0.4, "delta" => 0.002, "width" => 37, "SCpy_magnitude" => 0.4, "Attached_Leads" => "AB", "Lead_Coupling" => 1.0, "Lead_Width" => 0.5, "A__ChemPot" => 0.0, "A__Hopping" => 1.0, "A__Label" => "A", "A__Direction" => -1, "A__Contact" => (-1, 1), "A__Lead_Width" => 18, "A__Lead_Coupling" => 1.0, "A__SCbasis" => true, "B__ChemPot" => 0.0, "B__Hopping" => 1.0, "B__Label" => "B", "B__Direction" => 1, "B__Contact" => (1, -1), "B__Lead_Width" => 18, "B__Lead_Coupling" => 1.0, "B__SCbasis" => true)
 
 
-ComputeTasks.get_plot_one.(tasks)
+for task in tasks 
+
+	println()
+
+	@info task.name 
+
+add = ["Energy"=>0.157,"vec2scalar"=>"x","region"=>5, "obs_i"=>1,
+#					"transform"=>"Interpolate",
+#					"transform"=>"Fourier comp.","transfparam"=>1,
+#				 "transform"=>"|Fourier|",
+				 ]
+
+	out_dict = task.plot(Utils.adapt_merge(plot_P, add))
+														
+	println()
+
+	for (k,v) in pairs(out_dict)
+
+		println(k,"\t",(v isa String ? (v,) : (length(v)," ",typeof(v)))...)
+
+	end 
+
+end 
+
+
 
 
 #ComputeTasks.get_data_all(task, check_data=true, mute=false)
