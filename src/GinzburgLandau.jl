a37094e6d56ca02c6829e6bcae2c98b86a0b456f
tudor <tudor.pahomi@gmail.com>
2022-04-05 21:53:16 +0200

    compare GL energy

--- src/GinzburgLandau.jl
+++ src/GinzburgLandau.jl
@@ -1,17 +1,51 @@
 module GL#GinzburgLandau 
 #############################################################################
 
-import LinearAlgebra, Combinatorics
+import LinearAlgebra, Combinatorics, QuadGK 
+
+using myLibs.Parameters: UODict  
+
+import Helpers 
+using Constants: MAIN_DIM 
+
+import ..Lattice, ..Hamiltonian 
 
 
+function D4h_total_1D(get_eta::Function,
+											get_eta_Jacobian::Function,
+											(xmin,xmax)::Union{NTuple{2,<:Real},
+																				 AbstractVector{<:Real}},
+											args...;
+											kwargs...
+											)::Float64 
+
+	QuadGK.quadgk(xmin,xmax; kwargs...) do x
+
+		D4h_density(get_eta(x), get_eta_Jacobian(x), args...)
+
+	end[1]
+
+end 
+
+function D4h_density(eta::AbstractVector{<:Number},
+										 covJacob::AbstractMatrix{<:Number},
+										 a::Real,
+										 b::AbstractVector{<:Real},
+										 K::AbstractVector{<:Real},
+										 )::Float64
+
+	D4h_density_homog(eta,a,b) + D4h_density_grad(covJacob,K)
+
+end 
 
 
 function D4h_density_homog((x,y)::AbstractVector{<:Number},
 													 a::Real,
-													 b1::Real,
-													 b2::Real,
-													 b3::Real
+													 b::AbstractVector{<:Real},
 													)::Float64
+
+	@assert length(b)==3  
+
 	f::Float64 = 0.0 
 
 	n2x = abs2(x) 
@@ -21,11 +55,11 @@ function D4h_density_homog((x,y)::AbstractVector{<:Number},
 	
 	f += a*(n2x+n2y) 
 	
-	f += b1*(n2x+n2y)^2 
+	f += b[1]*(n2x+n2y)^2 
 
-	f += b2*2*real((conj(x)*y)^2)
+	f += b[2]*2*real((conj(x)*y)^2)
 
-	f += b3*n2x*n2y 
+	f += b[3]*n2x*n2y 
 
 	return f 
 
@@ -34,14 +68,14 @@ end
 
 
 function D4h_density_grad(D::AbstractMatrix{<:Number},
-													K1::Real,
-													K2::Real,
-													K3::Real,
-													K4::Real,
-													K5::Real,
+													K::AbstractVector{<:Real}
 												 )::Float64
 
 	@assert size(D) == (3,2) 
+
+	@assert 4<=length(K)<=5
+
+
 
 #	D[i,j] = D_i eta_j 
 
@@ -55,15 +89,17 @@ function D4h_density_grad(D::AbstractMatrix{<:Number},
 
 	for (i,j) in [(1,2),(2,1)]
 
-		f += K1*abs2(D[i,i]) # (abs2(DxNx) + abs2(DyNy))
+		f += K[1]*abs2(D[i,i]) # (abs2(DxNx) + abs2(DyNy))
 
-		f += K2*abs2(D[i,j]) # (abs2(DxNy) + abs2(DyNx))
+		f += K[2]*abs2(D[i,j]) # (abs2(DxNy) + abs2(DyNx))
 
-		f += K3*conj(D[i,i])*D[j,j] # 2*real(conj(DxNx)*DyNy)
+		f += K[3]*conj(D[i,i])*D[j,j] # 2*real(conj(DxNx)*DyNy)
 		
-		f += K4*conj(D[i,j])*D[j,i] # 2*real(conj(DxNy)*DyNx) 
+		f += K[4]*conj(D[i,j])*D[j,i] # 2*real(conj(DxNy)*DyNx) 
 
-		f += K5*abs2(D[3,i]) # (abs2(DzNx) + abs2(DzNy)) 
+		length(K)==5 || continue 
+
+		f += K[5]*abs2(D[3,i]) # (abs2(DzNx) + abs2(DzNy)) 
 
 	end 
 
@@ -106,8 +142,35 @@ end
 
 
 
+function bs_from_anisotropy(nu::Real,b::Real=1)::Vector{Float64}
+
+	[(3+nu)/8, (1-nu)/4, - (3nu+1)/4]*b
+
+end 
+
+function Ks_from_anisotropy(nu::Real,K::Real=1)::Vector{Float64}
+
+	vcat(3+nu, fill(1-nu,3), 0)*K/4 
+	
+end 
 
 
+
+function eta_path_length_1D(dev_params::UODict)::Vector{Float64}
+
+	lim = extrema(Helpers.Lattice.selectMainDim(Lattice.PosAtoms(dev_params)))
+
+	return map([1,2]) do component 
+
+		deriv = Hamiltonian.eta_Jacobian(dev_params,[MAIN_DIM],[component])
+
+		deriv isa AbstractMatrix && return 0.0 
+
+		return QuadGK.quadgk(LinearAlgebra.norm∘deriv, lim...; rtol=1e-10)[1]  
+
+	end 
+
+end  
 
 
 
--- test/Ginzburg_Landau.jl
+++ test/Ginzburg_Landau.jl
@@ -1,9 +1,11 @@
-#import Helpers , myPlots
+#import Helpers, myPlots
 #import myLibs:Parameters, Utils, ComputeTasks,Algebra
 #import Device
-
+import myLibs: Utils 
 import Device: GL 
-import PyPlot 
+import PyPlot,LinearAlgebra
+using Constants: VECTOR_STORE_DIM 
+import QuadGK 
 
 #t0 = init(Device, :Spectrum)
 
@@ -11,94 +13,185 @@ import PyPlot
 
 #ComputeTasks.get_data_one(t0; mute=false)   
 
+colors = ["brown","red","coral","peru","gold","olive","forestgreen","lightseagreen","dodgerblue","midnightblue","darkviolet","deeppink"]
+				 
+
+PyPlot.close()
+
+fig,(ax1,ax2) = PyPlot.subplots(1,2,figsize=(10,5))
+
+anisotropy = -0.6
+
+SCDW_shapes = [0,2,3,6]
+
+for (SCDW_shape,color) in zip(SCDW_shapes,Utils.Random_Items(colors,length(SCDW_shapes)))
+
+	for (ialpha,alpha) in enumerate(LinRange(0,1,250))
+
+		P = (SCDW_width = 0.15, 
+				 SCpx_magnitude = 0.1, Barrier_shape = 2, Barrier_width = 0.03, 
+		Barrier_height = 0, 
+		SCDW_shape = SCDW_shape,
+		SCDW_phasediff = alpha, 
+		length = 60, 
+		width = 30,
+		SCpy_magnitude = 0.1);
+		
+		#p = merge(t0.get_plotparams(P),Dict( "Energy"=>0.02,
+		##																			"obs_i"=>1,
+		#																			"filterstates"=>false,
+		#																			"oper"=>"PHzMirrorX",
+		#																			"smooth"=>0.1,
+		#																			)) 
+		#
+		
+		
+		
+		f_eta = Device.Hamiltonian.eta_interp(P)
+		
+		f_etaJ = Device.Hamiltonian.eta_interp_deriv(P)
+		
+		f_etaxy = Device.Hamiltonian.eta(P) 
+		
+		f_etaxy_Jacobian = Device.Hamiltonian.eta_Jacobian(P)
+		
+		
+		
+		#@show f_eta(-1) f_eta(0) f_eta(1) 
+		
+		#@show f_etaJ(-1) 
+		
+		b = 1 
+		
+		
+#		bs = [0.3b, 0.4b, 0.2b]
+		
+		bs = GL.bs_from_anisotropy(anisotropy,b) 
+		
+		#@show bs  
+		
+		#PyPlot.close() 
+		
+		eta_magn_sq = sum(abs2, f_eta(-1))
+	
+
+		a = - eta_magn_sq*b
+		
+		#@show a  
+		
+		coherence_length = 0.7/sqrt(eta_magn_sq/2)
+	
+		
+#		@show coherence_length Device.Hamiltonian.domain_wall_len(P)
+		
+		
+		K = 2b*eta_magn_sq*coherence_length  
+		
+		#@show K 
+		
+		
+#		Ks =[0.6K, 0.4K, 0.4K,0.4K,0.0K]
+		Ks=GL.Ks_from_anisotropy(anisotropy,K)
+		
+		nus = LinRange(-1, 1, 300) 
+		
+#		println("Likely anisotropy: ",nus[argmin([LinearAlgebra.norm(GL.bs_from_anisotropy(nu,b) .- bs) for nu in nus])])
+		
+		#@show bs GL.bs_from_anisotropy(-0.6,b) 
+		
+		
+		#@show Ks GL.Ks_from_anisotropy(-0.6,K)
+		#for (i,bis) in enumerate(zip(GL.bs_from_anisotropy.(nus,b)...))
+		#
+		#	PyPlot.plot(extrema(nus),fill(bs[i],2), label="b_$i^0")
+		#
+		#	PyPlot.plot(nus,bis,label="b_$i")
+		#
+		#end 
+		#
+		
+		
+		#PyPlot.legend()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		#@show GL.D4h_density_homog(f_etaxy(rand(2)),a,bs)
+		#																									
+		#@show GL.D4h_density_grad(f_etaxy_Jacobian(rand(2)),Ks)
+		
+		#X = LinRange(-1,1,100)
+		
+		X = LinRange((P.length *[-1,1]/2)...,200)
+		
+		XY = [[x,rand()] for x in X]
+		
+		
+		#PyPlot.close() 
+		
+		
+		#PyPlot.plot(X, [GL.D4h_density_homog(f_etaxy(x),a,bs) for x in X],label="homog")
+		
+		
+		
+		#PyPlot.plot(X, [GL.D4h_density_grad(f_etaxy_Jacobian(x),Ks) for x in X],label="grad")
+		
+		
+		xlim,ylim = extrema(Device.Lattice.PosAtoms(P),dims=VECTOR_STORE_DIM)
+
+		if ialpha==1 
+			println()
+			@show eta_magn_sq 
+			@show f_etaxy(xlim[1]) f_etaxy(0) f_etaxy(xlim[2])
+
+		end
 
 
-P = (SCDW_width = 0.15, 
-		 SCpx_magnitude = 0.4, Barrier_shape = 2, Barrier_width = 0.03, 
-Barrier_height = 0, 
-#SCDW_shape = 1,
-SCDW_phasediff = 0.4, 
-length = 60, 
-width = 30,
-SCpy_magnitude = 0.4);
+		kw = (c=color, label=ialpha==1 ? "Shape=$SCDW_shape" : nothing,s=6)
 
-#p = merge(t0.get_plotparams(P),Dict( "Energy"=>0.02,
-##																			"obs_i"=>1,
-#																			"filterstates"=>false,
-#																			"oper"=>"PHzMirrorX",
-#																			"smooth"=>0.1,
-#																			)) 
-#
+		ax1.scatter(alpha,
+								GL.D4h_total_1D(f_etaxy, f_etaxy_Jacobian, xlim, a, bs, Ks);
+								kw...)
 
+		ax2.scatter(alpha, sum(Device.Hamiltonian.eta_path_length(P)); kw...) 
 
+#		ax3.scatter(alpha, sum(GL.eta_path_length_1D(P)); kw...)
 
-f_eta = Device.Hamiltonian.eta_interp(P)
+		if ialpha==1 
+			ax1.set_title("GL free energy")
+			ax2.set_title("Total path length")
+#			ax3.set_title("Path length (x)")
+		end 
 
-f_etaJ = Device.Hamiltonian.eta_interp_deriv(P)
-
-f_etaxy = Device.Hamiltonian.eta(P) 
-
-f_etaxy_Jacobian = Device.Hamiltonian.eta_Jacobian(P)
+		ialpha%30==0 && sleep(0.005)
+		
+		
+		#PyPlot.plot(X, GL.D4h_density_homog.(f_eta.(X),a,b1,b2,b3),label=a) 
 
 
+	end 
+	ax1.legend(fontsize=12)
+	ax2.legend(fontsize=12)
 
-@show f_eta(-1) f_eta(0) f_eta(1) 
+	ax1.set_xlim(0,1)
+	ax2.set_xlim(0,1)
 
-@show f_etaJ(-1) 
+	fig.suptitle("\$\\nu=$anisotropy\$") 
 
-b = 1 
+	fig.tight_layout()	
+	fig.subplots_adjust(top=0.9) 
 
-bs = (b1, b2, b3) = (0.3b, 0.4b, 0.2b)
-
-K = 1
-
-Ks = (0.6K, 0.4K, 0.4K,0.4K,0.0K) 
-
-
-a = -0.1
-
-@show GL.D4h_density_homog(f_etaxy(rand(2)),a,bs...)
-																									
-@show GL.D4h_density_grad(f_etaxy_Jacobian(rand(2)),Ks...)
-
-#X = LinRange(-1,1,100)
-
-X = LinRange((P.length *[-1,1]/2)...,200)
-
-XY = [[x,rand()] for x in X]
-
-
-PyPlot.close() 
-
-
-PyPlot.plot(X, GL.D4h_density_homog.(f_etaxy.(XY),a,b1,b2,b3),label="homog")
-
-
-
-PyPlot.plot(X, GL.D4h_density_grad.(f_etaxy_Jacobian.(XY),Ks...),label="grad")
-
-using Constants: VECTOR_STORE_DIM 
-import QuadGK 
-
-xlim,ylim = extrema(Device.Lattice.PosAtoms(P),dims=VECTOR_STORE_DIM)
-
-
-q = QuadGK.quadgk(xlim...) do x 
-
-	GL.D4h_density_homog(f_etaxy([x,rand()]), a, bs...)+ GL.D4h_density_grad(f_etaxy_Jacobian([x,rand()]), Ks...)
 end 
 
-@show q 
 
-#PyPlot.plot(X, GL.D4h_density_homog.(f_eta.(X),a,b1,b2,b3),label=a) 
-
-
-
-PyPlot.legend()
-
-
-
-
-
-
-
+	
+	
+	
+	
+	
+	
