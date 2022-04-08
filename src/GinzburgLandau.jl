bd02263b26653006ddd4dc6b43069591235b79b2
tudor <tudor.pahomi@gmail.com>
2022-04-08 22:11:31 +0200

    working on GL derivatives

--- src/GinzburgLandau.jl
+++ src/GinzburgLandau.jl
@@ -3,6 +3,8 @@ module GL#GinzburgLandau
 
 import LinearAlgebra, Combinatorics, QuadGK 
 
+import myLibs: Utils 
+
 using myLibs.Parameters: UODict  
 
 import Helpers 
@@ -10,36 +12,78 @@ using Constants: MAIN_DIM
 
 import ..Lattice, ..Hamiltonian 
 
+other = Dict(1=>2,2=>1)
 
-function D4h_total_1D(get_eta::Function,
-											get_eta_Jacobian::Function,
-											(xmin,xmax)::Union{NTuple{2,<:Real},
-																				 AbstractVector{<:Real}},
-											args...;
-											kwargs...
-											)::Float64 
+rep(i::Int)::NTuple{2,Int} = (i,i)
+rep(t::NTuple{2,Int})::Vector{NTuple{2,Int}} = [t,t]
+rep(i::Int,j::Int)::Vector{NTuple{2,Int}} = rep((i,j)) 
 
-	QuadGK.quadgk(xmin,xmax; kwargs...) do x
+#()::Vector{Vector{NTuple{2,Int}}}
+D4h_homog_ord2 = [ (1,[(1,1),(2,2)]) ] 
 
-		D4h_density(get_eta(x), get_eta_Jacobian(x), args...)
 
-	end[1]
+#j											 )::Vector{Tuple{Float64,Vector{Vector{NTuple{2,Int}}}}}
+D4h_homog_ord4 = [
+	 (1,vcat([rep(rep(i)) for i=1:2], [rep(i,other[i]) for i=1:2])),
+	 (0.5,[[rep(i),rep(other[i])] for i=1:2]),
+	 (1,[rep(1,2)])
+	 ]
 
+
+
+#()::Vector{Tuple{Float64,Vector{Vector{NTuple{2,Int}}}}}
+D4h_grad_ord2 = [
+	 (1,[rep(rep(i)) for i=1:2]),
+	 (1,[rep(i,other[i]) for i=1:2]),
+	 (1,[[rep(i),rep(other[i])] for i=1:2]),
+	 (1,[[(i,other[i]),(other[i],i)] for i=1:2]),
+	 (1,[rep(3,i) for i=1:2])
+	]
+
+ignore_zero_imag(x::Real)::Real = x
+
+ignore_zero_imag(x::ComplexF64)::Real = abs(imag(x))<1e-12 ? real(x) : error() 
+
+function iterate_GL_terms(coeffs::AbstractVector{<:Real},
+													indsets::AbstractVector{<:Tuple{<:Real,<:AbstractVector}}
+													)::Base.Iterators.Flatten
+
+	@assert length(coeffs) == length(indsets)
+
+	((c1*c2,comb) for (c1,(c2,combs)) in zip(coeffs,indsets) for comb in combs)
+	
 end 
 
-function D4h_density(eta::AbstractVector{<:Number},
-										 covJacob::AbstractMatrix{<:Number},
-										 a::Real,
-										 b::AbstractVector{<:Real},
-										 K::AbstractVector{<:Real},
-										 )::Float64
 
-	D4h_density_homog(eta,a,b) + D4h_density_grad(covJacob,K)
+#function D4h_total_1D(get_eta::Function,
+#											get_eta_Jacobian::Function,
+#											(xmin,xmax)::Union{NTuple{2,<:Real},
+#																				 AbstractVector{<:Real}},
+#											args...;
+#											kwargs...
+#											)::Float64 
+#
+#	QuadGK.quadgk(xmin,xmax; kwargs...) do x
+#
+#		D4h_density(get_eta(x), get_eta_Jacobian(x), args...)
+#
+#	end[1]
+#
+#end 
 
-end 
+#function D4h_density(eta::AbstractVector{<:Number},
+#										 covJacob::AbstractMatrix{<:Number},
+#										 a::Real,
+#										 b::AbstractVector{<:Real},
+#										 K::AbstractVector{<:Real},
+#										 )::Float64
+#
+#	D4h_density_homog(eta,a,b) + D4h_density_grad(covJacob,K)
+#
+#end 
 
 
-function D4h_density_homog((x,y)::AbstractVector{<:Number},
+function D4h_density_homog_old((x,y)::AbstractVector{<:Number},
 													 a::Real,
 													 b::AbstractVector{<:Real},
 													)::Float64
@@ -57,7 +101,7 @@ function D4h_density_homog((x,y)::AbstractVector{<:Number},
 	
 	f += b[1]*(n2x+n2y)^2 
 
-	f += b[2]*2*real((conj(x)*y)^2)
+	f += b[2]*real((conj(x)*y)^2)
 
 	f += b[3]*n2x*n2y 
 
@@ -66,8 +110,178 @@ function D4h_density_homog((x,y)::AbstractVector{<:Number},
 end 
 
 
+function D4h_density_homog(eta::AbstractVector{<:Number},
+													 etac::AbstractVector{<:Number},
+														coeffs_ord2::AbstractVector{<:Real},
+														coeffs_ord4::AbstractVector{<:Real},
+													)::ComplexF64
 
-function D4h_density_grad(D::AbstractMatrix{<:Number},
+	f::ComplexF64 = 0.0 + 0.0im 
+
+	for (a,(i,j)) in iterate_GL_terms(coeffs_ord2,D4h_homog_ord2) 
+
+		f += a*eta[i]*etac[j]
+
+	end 
+
+	for (b,((i,j),(k,l))) in iterate_GL_terms(coeffs_ord4,D4h_homog_ord4) 
+
+		f += b * eta[i]*eta[j] * etac[k]*etac[l]
+
+	end 
+
+	return f 
+
+ 
+end 
+
+function D4h_density_homog(eta::AbstractVector{<:Number},
+														coeffs_ord2::AbstractVector{<:Real},
+														coeffs_ord4::AbstractVector{<:Real},
+													)::Float64
+
+	f = ignore_zero_imag(D4h_density_homog(eta, conj(eta), coeffs_ord2, coeffs_ord4))
+
+	@assert f ≈ D4h_density_homog_old(eta,coeffs_ord2[1],coeffs_ord4)
+
+	return f 
+
+end 
+
+
+function D4h_density_homog_deriv(eta::AbstractVector{<:Number},
+																 coeffs_ord2::AbstractVector{<:Real},
+																 coeffs_ord4::AbstractVector{<:Real},
+																 )::Vector{ComplexF64}
+
+	etac = conj(eta)
+
+	d = zeros(ComplexF64,2)
+
+	for (aij,(i,j)) in iterate_GL_terms(coeffs_ord2,D4h_homog_ord2) 
+
+		d[i] += aij*etac[j]
+
+	end 
+
+	for (b,((i,j),(k,l))) in iterate_GL_terms(coeffs_ord4,D4h_homog_ord4) 
+
+		d[i] += b * eta[j] * etac[k]*etac[l]
+		d[j] += b * eta[i] * etac[k]*etac[l]
+
+	end 
+
+	return d 
+
+end 
+
+
+function D4h_density_homog_deriv2(eta::AbstractVector{<:Number},
+																 coeffs_ord2::AbstractVector{<:Real},
+																 coeffs_ord4::AbstractVector{<:Real},
+																 )#::Vector{ComplexF64}
+
+	etac = conj(eta)
+
+	d = zeros(ComplexF64, 2, 2)
+
+	c = zeros(ComplexF64, 2, 2)
+
+
+#	d[i,j] = d^2 F/(d eta_j d eta_i) 
+#	derivatives are performed in the order of the indices 
+
+	for (aij,(i,j)) in iterate_GL_terms(coeffs_ord2,D4h_homog_ord2) 
+
+#		d[i,j] += 0
+		
+		c[j,i] += aij
+
+	end 
+
+	for (b,((i,j),(k,l))) in iterate_GL_terms(coeffs_ord4,D4h_homog_ord4) 
+
+		d[j,i] += b * etac[k]*etac[l]  
+		d[i,j] += b * etac[k]*etac[l]
+
+		c[k,i] += b * eta[j] * etac[l]  
+		c[l,i] += b * eta[j] * etac[k]
+
+	end 
+
+	return (d,c) 
+
+end 
+
+
+
+
+
+
+function D4h_density_homog_deriv(eta::AbstractVector{<:Number},
+																 etac::AbstractVector{<:Number},
+																 coeffs_ord2::AbstractVector{<:Real},
+																 coeffs_ord4::AbstractVector{<:Real},
+																 )::NTuple{2,Vector{ComplexF64}}
+	d = zeros(ComplexF64,2)
+	c = zeros(ComplexF64,2)
+
+	for (aij,(i,j)) in iterate_GL_terms(coeffs_ord2,D4h_homog_ord2) 
+
+		d[i] += aij*etac[j]
+		c[j] += aij*eta[i] 
+
+	end 
+
+	for (b,((i,j),(k,l))) in iterate_GL_terms(coeffs_ord4,D4h_homog_ord4) 
+
+		d[i] += b * eta[j] * etac[k]*etac[l]
+		d[j] += b * eta[i] * etac[k]*etac[l]
+
+		c[k] += b * eta[i]*eta[j] * etac[l]
+		c[l] += b * eta[i]*eta[j] * etac[k]
+
+	end 
+	
+#	F = D4h_density_homog(eta, coeffs_ord2, coeffs_ord4)
+#
+#	for step in Utils.logspace(1e-2,1e-10,20)
+#
+#		D = [D4h_density_homog(eta + [step,0], etac, coeffs_ord2,coeffs_ord4),
+#				 D4h_density_homog(eta + [0,step], etac, coeffs_ord2,coeffs_ord4),
+#				 ]
+#
+#		C = [
+#				 D4h_density_homog(eta, etac + [step,0], coeffs_ord2,coeffs_ord4),
+#				 D4h_density_homog(eta, etac + [0,step], coeffs_ord2,coeffs_ord4),
+#				 ]
+#
+#		ords = ord, ord_d, ord_c = -log10.([step,
+#						 LinearAlgebra.norm(d-(D .- F)/step),
+#						 LinearAlgebra.norm(c-(C .- F)/step)]) 
+#
+#		@assert ord_d > ord/2 
+#		@assert ord_c > ord/2 
+#
+##		println(join(round.(ords, digits=1),"\t"))
+#
+#	end  
+
+	return d,c
+ 
+end 
+
+
+
+
+
+
+
+
+
+
+
+function D4h_density_grad_old(D::AbstractMatrix{<:Number},
 													K::AbstractVector{<:Real}
 												 )::Float64
 
@@ -85,7 +299,7 @@ function D4h_density_grad(D::AbstractMatrix{<:Number},
 
 #	DzNx,DzNy = D[3,:]
 
-	f::Float64 = 0.0 
+	f = 0.0 
 
 	for (i,j) in [(1,2),(2,1)]
 
@@ -103,13 +317,171 @@ function D4h_density_grad(D::AbstractMatrix{<:Number},
 
 	end 
 
-	return f 
+
+	return ignore_zero_imag(f)
 
 end 
 
 
-function covariant_derivative(eta::AbstractVector{<:Number},
+
+
+
+
+function D4h_density_grad(D::AbstractMatrix{<:Number},
+													Dc::AbstractMatrix{<:Number},
+													coeffs_ord2::AbstractVector{<:Real}
+													 )::ComplexF64
+
+	@assert size(D) == size(Dc) == (3,2) 
+
+	f::ComplexF64 = 0.0 + 0.0im
+
+	for (k,(I1,I2)) in iterate_GL_terms(coeffs_ord2, D4h_grad_ord2)
+
+		f += k * D[I1...] * Dc[I2...]
+
+	end 
+
+	return f 
+
+end 
+
+function D4h_density_grad(D::AbstractMatrix{<:Number},
+													coeffs_ord2::AbstractVector{<:Real}
+													 )::Float64
+	
+#	@assert D4h_density_grad_old(D,coeffs_ord2)≈ignore_zero_imag(f) 
+
+	ignore_zero_imag(D4h_density_grad(D,conj(D),coeffs_ord2))
+
+end 
+
+function D4h_density_grad_deriv(D::AbstractMatrix{<:Number},
+													coeffs_ord2::AbstractVector{<:Real}
+													)::Matrix{ComplexF64}
+
+	Dc = conj(D)
+
+#	D4h_density_grad_deriv(D, conj(D), coeffs_ord2)
+
+	@assert size(D) == (3,2) 
+
+	d = zeros(ComplexF64,3,2)
+
+
+	for (k,(I1,I2)) in iterate_GL_terms(coeffs_ord2, D4h_grad_ord2)
+
+		d[I1...] += k * Dc[I2...]
+
+	end  
+
+	return d 
+
+end 
+
+function D4h_density_grad_deriv(D::AbstractMatrix{<:Number},
+													Dc::AbstractMatrix{<:Number},
+													coeffs_ord2::AbstractVector{<:Real}
+													)::NTuple{2,Matrix{ComplexF64}}
+
+	@assert size(D) == size(Dc) == (3,2) 
+
+	d = zeros(ComplexF64,3,2)
+	c = zeros(ComplexF64,3,2)
+
+
+	for (k,(I1,I2)) in iterate_GL_terms(coeffs_ord2, D4h_grad_ord2)
+
+#		f += k * D[I1...] * Dc[I2...]
+
+		d[I1...] += k * Dc[I2...]
+
+		c[I2...] += k * D[I1...]
+
+	end 
+
+	
+#	G = D4h_density_grad(D,Dc,coeffs_ord2)
+#
+#	for step in Utils.logspace(1e-2,1e-10,20)
+#
+#		d2 = zeros(ComplexF64,3,2)
+#		c2 = zeros(ComplexF64,3,2)
+#	
+#		for i=1:3,j=1:2 
+#	
+#			x = zeros(ComplexF64,3,2)
+#	
+#			x[i,j] += step 
+#			
+#			d2[i,j] = (D4h_density_grad(D+x,Dc,coeffs_ord2)-G)/step
+#	
+#			c2[i,j] = (D4h_density_grad(D,Dc+x,coeffs_ord2)-G)/step
+#	
+#		end 
+#
+#		ords = ord, ord_d, ord_c = -log10.([step, 
+#																				LinearAlgebra.norm(d - d2),
+#																				LinearAlgebra.norm(c - c2)])
+#	
+#	
+#	
+#	
+#			@assert ord_d > ord/2 
+#			@assert ord_c > ord/2 
+#	
+#			println(join(round.(ords, digits=1),"\t"))
+#	
+#	end  
+
+
+	return (d,c)
+
+end 
+
+
+
+
+
+
+
+
+
+function BCS_coherence_length(Delta0::Real)::Float64
+
+	0.7/Delta0
+
+end  
+
+function eta_magnitude_squared(Delta0::Real)::Float64 
+
+	2*Delta0^2
+
+end 
+
+function get_K_ampl(Delta0::Real,b::Real=1)::Float64 
+
+	2b*eta_magnitude_squared(Delta0)*BCS_coherence_length(Delta0) 
+
+end 
+
+function get_coeff_a(Delta0::Real,b::Real=1)::Vector{Float64}
+
+	[-eta_magnitude_squared(Delta0)*b]
+
+end 
+
+
+function covariant_derivative(etaJacobian::T
+															)::T where T<:AbstractMatrix{<:Number}
+	etaJacobian 
+
+end  
+
+
+function covariant_derivative(
 															etaJacobian::AbstractMatrix{<:Number},
+															eta::AbstractVector{<:Number},
 															A::AbstractVector{<:Number},
 															gamma::Real,
 															)::Matrix{ComplexF64}
@@ -119,6 +491,88 @@ function covariant_derivative(eta::AbstractVector{<:Number},
 	etaJacobian + im*gamma*A*transpose(eta)
 
 end 
+
+
+function covariant_derivative_deriv_eta(
+															etaJacobian::AbstractMatrix{<:Number},
+															eta::AbstractVector{<:Number},
+															A::AbstractVector{<:Number},
+															gamma::Real,
+															)::NTuple{2,Matrix{ComplexF64}}
+
+	@assert size(etaJacobian)==(3,2)
+
+	(A*[im*gamma 0], A*[0 im*gamma])
+
+end 
+
+
+function covariant_derivative_deriv_A(
+															etaJacobian::AbstractMatrix{<:Number},
+															eta::AbstractVector{<:Number},
+															A::AbstractVector{<:Number},
+															gamma::Real,
+															)::NTuple{3,Matrix{ComplexF64}}
+
+	@assert size(etaJacobian)==(3,2)
+
+	([im*gamma 0 0]*transpose(eta), 
+	 [0 im*gamma 0]*transpose(eta), 
+	 [0 0 im*gamma]*transpose(eta)
+	 )
+
+end 
+
+
+
+
+
+function chain_rule_outer(dF_df::AbstractVector{T1},
+										df_dx::AbstractVector{T2}
+										)::Matrix{promote_type(T1,T2)} where {T1<:Number,T2<:Number}
+
+	df_dx * transpose(dF_df)
+
+end 
+
+
+
+
+
+function chain_rule_inner(dF_df::AbstractVector{T1},
+										df_dx::AbstractVector{T2}
+										)::promote_type(T1,T2) where {T1<:Number,T2<:Number}
+
+	mapreduce(*,+,dF_df,df_dx;init=0.0)
+
+end 
+
+function chain_rule_inner(dF_df::AbstractMatrix{T1},
+										df_dx::AbstractVector{T2}
+										)::Vector{promote_type(T1,T2)} where {T1<:Number,T2<:Number}
+
+	dF_df*df_dx
+
+end 
+
+function chain_rule_inner(A::AbstractVecOrMat,B::AbstractVecOrMat,
+													a::AbstractVector,b::AbstractVector
+													)
+	chain_rule_inner(A,a) + chain_rule_inner(B,b)
+																	
+end  
+
+
+
+chain_rule_inner(t::Tuple) = chain_rule_inner(t...)
+chain_rule_inner(t1::Tuple,t2::Tuple) = chain_rule_inner(t1...,t2...)
+
+
+
+	
+
+
+
 
 
 
@@ -148,39 +602,176 @@ function bs_from_anisotropy(nu::Real,b::Real=1)::Vector{Float64}
 
 end 
 
-function Ks_from_anisotropy(nu::Real,K::Real=1)::Vector{Float64}
+function Ks_from_anisotropy(nu::Real,K::Real)::Vector{Float64}
 
 	vcat(3+nu, fill(1-nu,3), 0)*K/4 
 	
 end 
 
+function get_coeffs_Ks(nu::Real,Delta0::Real,b::Real=1)::Vector{Float64}
 
-
-function eta_path_length_1D(dev_params::UODict)::Vector{Float64}
-
-	lim = extrema(Helpers.Lattice.selectMainDim(Lattice.PosAtoms(dev_params)))
-
-	return map([1,2]) do component 
-
-		deriv = Hamiltonian.eta_Jacobian(dev_params,[MAIN_DIM],[component])
-
-		deriv isa AbstractMatrix && return 0.0 
-
-		return QuadGK.quadgk(LinearAlgebra.norm∘deriv, lim...; rtol=1e-10)[1]  
-
-	end 
+	Ks_from_anisotropy(nu,GL.get_K_ampl(Delta0,b))
 
 end  
 
 
 
 
+#function eta_path_length_1D(dev_params::UODict)::Vector{Float64}
+#
+#	lim = extrema(Helpers.Lattice.selectMainDim(Lattice.PosAtoms(dev_params)))
+#
+#	return map([1,2]) do component 
+#
+#		deriv = Hamiltonian.eta_Jacobian(dev_params,[MAIN_DIM],[component])
+#
+#		deriv isa AbstractMatrix && return 0.0 
+#
+#		return QuadGK.quadgk(LinearAlgebra.norm∘deriv, lim...; rtol=1e-10)[1]  
+#
+#	end 
+#
+#end  
+
+
+
+function D4h_density_1D(P::UODict)::Function 
+
+	f_eta = Hamiltonian.eta_interp(P)
+			
+	f_etaJ = Hamiltonian.eta_interp_deriv(P)
+	
+	Delta0 = real(only(unique(Hamiltonian.eta_magnitudes(P))))
+	
+	anisotropy = 0#-0.6
+	
+	a = GL.get_coeff_a(Delta0)
+	
+	b = GL.bs_from_anisotropy(anisotropy)
+	
+	K = GL.get_coeffs_Ks(anisotropy,Delta0)
+
+#		A = zeros(3)
+#		gamma = 0
+
+	function out(g::Real,grad_g::AbstractVector{<:Real} 
+							 )::Vector{Float64}
+
+		N = f_eta(g) 
+		
+		dN_dg = f_etaJ(g)
+		
+		dN_dx = chain_rule_outer(dN_dg, grad_g) # already covariant 
+		
+
+		F = D4h_density_homog(N,a,b) +  D4h_density_grad(dN_dx,K) 
+
+
+		dF_dg = chain_rule_inner(D4h_density_homog_deriv(N, a, b), dN_dg)
+
+		dF_dgx,dF_dgy = chain_rule_inner(D4h_density_grad_deriv(dN_dx,K), dN_dg) 
+
+		return [F, 2real(dF_dg), 2real(dF_dgx), 2real(dF_dgy)]
 
 
 
 
 
+	end 
+	 
 
+
+
+
+end 
+
+
+#===========================================================================#
+#
+#
+#
+#---------------------------------------------------------------------------#
+
+
+function dAdg(M::AbstractMatrix{Tm},
+							X::AbstractMatrix{Tx},
+							Y::AbstractMatrix{Ty},
+							h::Float64,s::Float64
+							) where {Tm<:Number,Tx<:Number,Ty<:Number}
+
+	n,m = size(M)
+
+	D = zeros(promote_type(Tm,Tx,Ty), n+1,m+1)
+
+	for (j,cols_j) in enumerate(zip(eachcol(M),eachcol(X),eachcol(Y))) 
+
+		for (i,(m,x,y)) in enumerate(zip(cols_j...))
+
+			D[i,j] += m - x - y
+
+			D[i+1,j] += m + x - y 
+
+			D[i,j+1] += m - x + y 
+
+			D[i+1,j+1] += m + x + y 
+
+		end 
+		
+	end 
+
+	D .*= s*h 
+
+	return D 
+
+end 
+
+
+#function gij_matrix(g::Function,xs::AbstractVector,ys::AbstractVector)
+#
+#
+#
+#end 
+
+function m_dx_dy(g::AbstractMatrix{<:Number},h::Real,s::Real
+								 )::NTuple{3,Matrix}
+
+	n,m = size(g) 
+
+
+	M = fill(0g[1] + 0.25, n-1, m-1)
+	
+	dX = fill(0g[1] + h/2, n-1, m-1)
+	
+	dY = fill(0g[1] + s/2, n-1, m-1)
+	
+
+
+	for j=1:m-1, i=1:n-1 
+
+		M[i,j]  *=  g[i,j] + g[i+1,j] + g[i,j+1] + g[i+1,j+1]
+		dX[i,j] *= -g[i,j] + g[i+1,j] - g[i,j+1] + g[i+1,j+1]
+		dY[i,j] *= -g[i,j] - g[i+1,j] + g[i,j+1] + g[i+1,j+1]
+
+	end 
+	
+	return M,dX,dY
+
+end 
+
+
+
+function middle_Riemann_sum(F::Function,
+														M::AbstractMatrix{<:Number},
+														dX::AbstractMatrix{<:Number},
+														dY::AbstractMatrix{<:Number},
+														h::Real, s::Real,
+														)::Float64
+
+	h*s*mapreduce(F, +, M, dX, dY; init=0.0)
+
+	return f 
+
+end 
 
 
 
--- test/Ginzburg_Landau.jl
+++ test/Ginzburg_Landau.jl
@@ -5,7 +5,7 @@ import myLibs: Utils
 import Device: GL 
 import PyPlot,LinearAlgebra
 using Constants: VECTOR_STORE_DIM 
-import QuadGK 
+import QuadGK ,Random
 
 #t0 = init(Device, :Spectrum)
 
@@ -13,181 +13,262 @@ import QuadGK
 
 #ComputeTasks.get_data_one(t0; mute=false)   
 
-colors = ["brown","red","coral","peru","gold","olive","forestgreen","lightseagreen","dodgerblue","midnightblue","darkviolet","deeppink"]
-				 
+#Random.seed!(561) 
+
+colors = ["brown","red","coral","peru","gold","olive","forestgreen","lightseagreen","dodgerblue","midnightblue","darkviolet","deeppink"] |> Random.shuffle
+	
+
+
+
+@testset "Derivatives of F" begin 
+
+	for trial in 1:10
+	
+		D = rand(ComplexF64,3,2)
+	
+		eta = rand(ComplexF64,2)
+	
+		K = rand(5) 
+	
+		a = rand(1) 
+	
+		b = rand(3) 
+	
+	#	@time begin for i=1:100 GL.D4h_density_grad(D,K) end end 
+	
+	#	@time begin for i=1:100 GL.D4h_density_grad2(D,K) end end 
+	
+	
+	#	GL.D4h_density_grad(D,K)#≈GL.D4h_density_grad2(D,K) 
+	#	GL.D4h_density_homog(eta,a,b)#≈GL.D4h_density_homog2(eta,a,b)
+	
+	#	GL.D4h_density_homog_deriv(eta,a,b)
+	
+		q1,q2 = GL.D4h_density_homog_deriv(eta,conj(eta),a,b)
+	
+		@test q1≈conj(q2)
+		
+		@test q1≈GL.D4h_density_homog_deriv(eta,a,b)
+	
+	
+		Q1,Q2 = GL.D4h_density_grad_deriv(D,conj(D),K) 
+	
+		@test Q1≈conj(Q2)
+		@test Q1≈GL.D4h_density_grad_deriv(D,K)
+
+		dd,cd = GL.D4h_density_homog_deriv2(eta,a,b)
+		
+		dc,cc = GL.D4h_density_homog_deriv2(conj(eta),a,b)
+
+
+		println(cd[1,2]," ",cd[2,1])
+		println(cc[1,2]," ",cc[2,1])
+		println(dc[1,2]," ",dc[2,1])
+		println(dd[1,2]," ",dd[2,1])
+
+		@show conj(cc)≈ cd
+		@show conj(dd)≈ dc
+
+		break 
+	end  
+
+end 
+
+
+error("end")
+
+println()
+
+
+P = (
+#		 length = 60,
+#		 Barrier_shape = 2, Barrier_width = 0.03, 
+#Barrier_height = 0, 
+
+SCDW_shape = 3,
+SCDW_phasediff = 0.8,
+#		 SCDW_width = 0.15, 
+		 SCpx_magnitude = 0.1, 
+);
+
+P = Device.Hamiltonian.adjust_paramcomb(1, P)
+#Device.Lattice.adjust_paramcomb(1, P))
+
+@show P 
+
+
+
+
+@show GL.D4h_density_1D(P)(2rand()-1,rand(3))
+
+
+		
+
+
 
 PyPlot.close()
 
 fig,(ax1,ax2) = PyPlot.subplots(1,2,figsize=(10,5))
 
-anisotropy = -0.6
 
 SCDW_shapes = [0,2,3,6]
 
-for (SCDW_shape,color) in zip(SCDW_shapes,Utils.Random_Items(colors,length(SCDW_shapes)))
-
-	for (ialpha,alpha) in enumerate(LinRange(0,1,250))
-
-		P = (SCDW_width = 0.15, 
-				 SCpx_magnitude = 0.1, Barrier_shape = 2, Barrier_width = 0.03, 
-		Barrier_height = 0, 
-		SCDW_shape = SCDW_shape,
-		SCDW_phasediff = alpha, 
-		length = 60, 
-		width = 30,
-		SCpy_magnitude = 0.1);
-		
-		#p = merge(t0.get_plotparams(P),Dict( "Energy"=>0.02,
-		##																			"obs_i"=>1,
-		#																			"filterstates"=>false,
-		#																			"oper"=>"PHzMirrorX",
-		#																			"smooth"=>0.1,
-		#																			)) 
-		#
-		
-		
-		
-		f_eta = Device.Hamiltonian.eta_interp(P)
-		
-		f_etaJ = Device.Hamiltonian.eta_interp_deriv(P)
-		
-		f_etaxy = Device.Hamiltonian.eta(P) 
-		
-		f_etaxy_Jacobian = Device.Hamiltonian.eta_Jacobian(P)
-		
-		
-		
-		#@show f_eta(-1) f_eta(0) f_eta(1) 
-		
-		#@show f_etaJ(-1) 
-		
-		b = 1 
-		
-		
-#		bs = [0.3b, 0.4b, 0.2b]
-		
-		bs = GL.bs_from_anisotropy(anisotropy,b) 
-		
-		#@show bs  
-		
-		#PyPlot.close() 
-		
-		eta_magn_sq = sum(abs2, f_eta(-1))
-	
-
-		a = - eta_magn_sq*b
-		
-		#@show a  
-		
-		coherence_length = 0.7/sqrt(eta_magn_sq/2)
-	
-		
-#		@show coherence_length Device.Hamiltonian.domain_wall_len(P)
-		
-		
-		K = 2b*eta_magn_sq*coherence_length  
-		
-		#@show K 
-		
-		
-#		Ks =[0.6K, 0.4K, 0.4K,0.4K,0.0K]
-		Ks=GL.Ks_from_anisotropy(anisotropy,K)
-		
-		nus = LinRange(-1, 1, 300) 
-		
-#		println("Likely anisotropy: ",nus[argmin([LinearAlgebra.norm(GL.bs_from_anisotropy(nu,b) .- bs) for nu in nus])])
-		
-		#@show bs GL.bs_from_anisotropy(-0.6,b) 
-		
-		
-		#@show Ks GL.Ks_from_anisotropy(-0.6,K)
-		#for (i,bis) in enumerate(zip(GL.bs_from_anisotropy.(nus,b)...))
-		#
-		#	PyPlot.plot(extrema(nus),fill(bs[i],2), label="b_$i^0")
-		#
-		#	PyPlot.plot(nus,bis,label="b_$i")
-		#
-		#end 
-		#
-		
-		
-		#PyPlot.legend()
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		#@show GL.D4h_density_homog(f_etaxy(rand(2)),a,bs)
-		#																									
-		#@show GL.D4h_density_grad(f_etaxy_Jacobian(rand(2)),Ks)
-		
-		#X = LinRange(-1,1,100)
-		
-		X = LinRange((P.length *[-1,1]/2)...,200)
-		
-		XY = [[x,rand()] for x in X]
-		
-		
-		#PyPlot.close() 
-		
-		
-		#PyPlot.plot(X, [GL.D4h_density_homog(f_etaxy(x),a,bs) for x in X],label="homog")
-		
-		
-		
-		#PyPlot.plot(X, [GL.D4h_density_grad(f_etaxy_Jacobian(x),Ks) for x in X],label="grad")
-		
-		
-		xlim,ylim = extrema(Device.Lattice.PosAtoms(P),dims=VECTOR_STORE_DIM)
-
-		if ialpha==1 
-			println()
-			@show eta_magn_sq 
-			@show f_etaxy(xlim[1]) f_etaxy(0) f_etaxy(xlim[2])
-
-		end
+#Utils.Random_Items(colors,length(SCDW_shapes))
 
 
-		kw = (c=color, label=ialpha==1 ? "Shape=$SCDW_shape" : nothing,s=6)
-
-		ax1.scatter(alpha,
-								GL.D4h_total_1D(f_etaxy, f_etaxy_Jacobian, xlim, a, bs, Ks);
-								kw...)
-
-		ax2.scatter(alpha, sum(Device.Hamiltonian.eta_path_length(P)); kw...) 
-
-#		ax3.scatter(alpha, sum(GL.eta_path_length_1D(P)); kw...)
-
-		if ialpha==1 
-			ax1.set_title("GL free energy")
-			ax2.set_title("Total path length")
-#			ax3.set_title("Path length (x)")
-		end 
-
-		ialpha%30==0 && sleep(0.005)
-		
-		
-		#PyPlot.plot(X, GL.D4h_density_homog.(f_eta.(X),a,b1,b2,b3),label=a) 
-
-
-	end 
-	ax1.legend(fontsize=12)
-	ax2.legend(fontsize=12)
-
-	ax1.set_xlim(0,1)
-	ax2.set_xlim(0,1)
-
-	fig.suptitle("\$\\nu=$anisotropy\$") 
-
-	fig.tight_layout()	
-	fig.subplots_adjust(top=0.9) 
-
-end 
-
+#for (SCDW_shape,color) in zip(SCDW_shapes,colors[1:length(SCDW_shapes)])
+#
+#	for (ialpha,alpha) in enumerate(LinRange(0,1,250))
+#
+#		P = (SCDW_width = 0.15, 
+#				 SCpx_magnitude = 0.1, Barrier_shape = 2, Barrier_width = 0.03, 
+#		Barrier_height = 0, 
+#		SCDW_shape = SCDW_shape,
+#		SCDW_phasediff = alpha, 
+#		length = 60, 
+#		width = 30,
+#		SCpy_magnitude = 0.1);
+#		
+#		#p = merge(t0.get_plotparams(P),Dict( "Energy"=>0.02,
+#		##																			"obs_i"=>1,
+#		#																			"filterstates"=>false,
+#		#																			"oper"=>"PHzMirrorX",
+#		#																			"smooth"=>0.1,
+#		#																			)) 
+#		#
+#		
+#		
+#		
+#		f_eta = Device.Hamiltonian.eta_interp(P)
+#		
+#		f_etaJ = Device.Hamiltonian.eta_interp_deriv(P)
+#		
+#		f_etaxy = Device.Hamiltonian.eta(P) 
+#		
+#		f_etaxy_Jacobian = Device.Hamiltonian.eta_Jacobian(P)
+#		
+#		
+#		
+#		
+#		
+#		
+##		bs = [0.3b, 0.4b, 0.2b]
+#		
+#		
+#		#@show bs  
+#		
+#		#PyPlot.close() 
+#		
+#		eta_magn_sq = sum(abs2, f_eta(-1))
+#	
+#
+#		
+#		#@show a  
+#		
+##		@show coherence_length Device.Hamiltonian.domain_wall_len(P)
+#		
+#		
+#		
+#		#@show K 
+#		
+#		
+##		Ks =[0.6K, 0.4K, 0.4K,0.4K,0.0K]
+#		
+#		nus = LinRange(-1, 1, 300) 
+#		
+##		println("Likely anisotropy: ",nus[argmin([LinearAlgebra.norm(GL.bs_from_anisotropy(nu,b) .- bs) for nu in nus])])
+#		
+#		#@show bs GL.bs_from_anisotropy(-0.6,b) 
+#		
+#		
+#		#@show Ks GL.Ks_from_anisotropy(-0.6,K)
+#		#for (i,bis) in enumerate(zip(GL.bs_from_anisotropy.(nus,b)...))
+#		#
+#		#	PyPlot.plot(extrema(nus),fill(bs[i],2), label="b_$i^0")
+#		#
+#		#	PyPlot.plot(nus,bis,label="b_$i")
+#		#
+#		#end 
+#		#
+#		
+#		
+#		#PyPlot.legend()
+#		
+#		
+#		
+#		
+#		
+#		
+#		
+#		
+#		
+#		#@show GL.D4h_density_homog(f_etaxy(rand(2)),a,bs)
+#		#																									
+#		#@show GL.D4h_density_grad(f_etaxy_Jacobian(rand(2)),Ks)
+#		
+#		#X = LinRange(-1,1,100)
+#		
+#		X = LinRange((P.length *[-1,1]/2)...,200)
+#		
+#		XY = [[x,rand()] for x in X]
+#		
+#		
+#		#PyPlot.close() 
+#		
+#		
+#		#PyPlot.plot(X, [GL.D4h_density_homog(f_etaxy(x),a,bs) for x in X],label="homog")
+#		
+#		
+#		
+#		#PyPlot.plot(X, [GL.D4h_density_grad(f_etaxy_Jacobian(x),Ks) for x in X],label="grad")
+#		
+#		
+#		xlim,ylim = extrema(Device.Lattice.PosAtoms(P),dims=VECTOR_STORE_DIM)
+#
+#		if ialpha==1 
+#			println()
+#			@show eta_magn_sq 
+#			@show f_etaxy(xlim[1]) f_etaxy(0) f_etaxy(xlim[2])
+#
+#		end
+#
+#
+#		kw = (c=color, label=ialpha==1 ? "Shape=$SCDW_shape" : nothing,s=6)
+#
+#		ax1.scatter(alpha,
+#								GL.D4h_total_1D(f_etaxy, f_etaxy_Jacobian, xlim, a, bs, Ks);
+#								kw...)
+#
+#		ax2.scatter(alpha, sum(Device.Hamiltonian.eta_path_length(P)); kw...) 
+#
+##		ax3.scatter(alpha, sum(GL.eta_path_length_1D(P)); kw...)
+#
+#		if ialpha==1 
+#			ax1.set_title("GL free energy")
+#			ax2.set_title("Total path length")
+##			ax3.set_title("Path length (x)")
+#		end 
+#
+#		ialpha%30==0 && sleep(0.005)
+#		
+#		
+#		#PyPlot.plot(X, GL.D4h_density_homog.(f_eta.(X),a,b1,b2,b3),label=a) 
+#
+#
+#	end 
+#	ax1.legend(fontsize=12)
+#	ax2.legend(fontsize=12)
+#
+#	ax1.set_xlim(0,1)
+#	ax2.set_xlim(0,1)
+#
+#	fig.suptitle("\$\\nu=$anisotropy\$") 
+#
+#	fig.tight_layout()	
+#	fig.subplots_adjust(top=0.9) 
+#
+#end 
+#
 
 	
 	
